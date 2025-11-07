38b043b7894109e3571547b973419c1309497e47
Luisss00 <luis2cop@gmail.com>
2025-11-06 22:05:21 -0500

    pagina en nube git

--- fashion-store/frontend/vite.config.js
+++ fashion-store/frontend/vite.config.js
@@ -3,14 +3,15 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/', // 👈 clave para GitHub Pages en repo Luisss00.github.io
   server: {
-    port: 3000,  
-    strictPort: true,  
+    port: 3000,
+    strictPort: true,
     proxy: {
       '/api': {
         target: 'http://localhost:8000',
         changeOrigin: true,
-      }
-    }
-  }
+      },
+    },
+  },
 })
